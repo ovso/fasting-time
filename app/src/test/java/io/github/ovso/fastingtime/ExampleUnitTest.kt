d7632d442e1f8f6fc112cf7e6b5bef37ce211188
jaeho.5 <ovsoce@gmail.com>
2023-11-04 17:31:22 +0900

    test: 하샤드 코딩 테스트

--- app/src/test/java/io/github/ovso/fastingtime/ExampleUnitTest.kt
+++ app/src/test/java/io/github/ovso/fastingtime/ExampleUnitTest.kt
@@ -13,5 +13,16 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        val answer = Solution().solution(120)
+        assert(answer)
+    }
+}
+
+class Solution {
+    fun solution(x: Int): Boolean {
+        val arr = x.toString().toCharArray().map { it.toString().toInt() }
+        val sum = arr.sum()
+        println(sum)
+        return x % sum == 0
     }
 }
