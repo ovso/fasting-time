1be4051c704e73a8c3b862e9ab9521885f464ff6
jaeho.5 <ovsoce@gmail.com>
2023-11-07 05:38:55 +0900

    feat: DropdownMenu 구현

--- app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
+++ app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
@@ -3,19 +3,33 @@ package io.github.ovso.fastingtime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedAssistChip
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.ovso.fastingtime.ui.theme.FastingtimeTheme
 
@@ -35,11 +49,42 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    var expand by remember { mutableStateOf(false) }
-    Column(modifier = Modifier.fillMaxSize()) {
 
+    var expanded by remember { mutableStateOf(false) }
+    var label by remember { mutableStateOf("15 시간 후 알림") }
+
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopStart)
+    ) {
+        ElevatedAssistChip(
+            onClick = { expanded = true },
+            label = { Text(label) },
+            leadingIcon = {
+                Icon(imageVector = Icons.Default.Notifications, contentDescription = null)
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                text = { Text("15 시간 후 알림") },
+                onClick = { label = "Edit" },
+            )
+            DropdownMenuItem(
+                text = { Text("Settings") },
+                onClick = { label = "Settings" },
+            )
+            Divider()
+            DropdownMenuItem(
+                text = { Text("Send Feedback") },
+                onClick = { label = "Send Feedback" },
+            )
+        }
     }
 }
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
