8dc46e8a0560c7379e622141db643bc3953648f1
jaeho.5 <ovsoce@gmail.com>
2023-11-07 10:20:25 +0900

    feat: DropdownMenu 구현 중

--- app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
+++ app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
@@ -3,22 +3,18 @@ package io.github.ovso.fastingtime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedAssistChip
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,7 +25,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.ovso.fastingtime.ui.theme.FastingtimeTheme
 
@@ -50,37 +45,75 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
 
-    var expanded by remember { mutableStateOf(false) }
+    var startExpanded by remember { mutableStateOf(false) }
+    var endExpanded by remember { mutableStateOf(false) }
     var label by remember { mutableStateOf("15 시간 후 알림") }
 
-    Box(
-        modifier = Modifier
-            .wrapContentSize(Alignment.TopStart)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ElevatedAssistChip(
-            onClick = { expanded = true },
-            label = { Text(label) },
-            leadingIcon = {
-                Icon(imageVector = Icons.Default.Notifications, contentDescription = null)
-            }
-        )
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
+
+        Text(text = "공복 시작", style = MaterialTheme.typography.headlineLarge)
+        Box(
+            modifier = Modifier
+                .wrapContentSize(Alignment.TopStart)
         ) {
-            DropdownMenuItem(
-                text = { Text("15 시간 후 알림") },
-                onClick = { label = "Edit" },
+            ElevatedAssistChip(
+                onClick = { startExpanded = true },
+                label = { Text(label) },
             )
-            DropdownMenuItem(
-                text = { Text("Settings") },
-                onClick = { label = "Settings" },
+            DropdownMenu(
+                expanded = startExpanded,
+                onDismissRequest = { startExpanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("00:30") },
+                    onClick = { label = "00 시" },
+                )
+                DropdownMenuItem(
+                    text = { Text("00 시") },
+                    onClick = { label = "00 시" },
+                )
+                Divider()
+                DropdownMenuItem(
+                    text = { Text("00 시") },
+                    onClick = { label = "00 시" },
+                )
+            }
+        }
+
+        Text(text = "공복 그만", style = MaterialTheme.typography.headlineLarge)
+        Box(
+            modifier = Modifier
+                .wrapContentSize(Alignment.TopStart)
+        ) {
+            ElevatedAssistChip(
+                onClick = { endExpanded = true },
+                label = { Text(label) },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = null)
+                }
             )
-            Divider()
-            DropdownMenuItem(
-                text = { Text("Send Feedback") },
-                onClick = { label = "Send Feedback" },
-            )
+            DropdownMenu(
+                expanded = endExpanded,
+                onDismissRequest = { endExpanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("15 시간 후 알림") },
+                    onClick = { label = "15 시간 후 알림" },
+                )
+                DropdownMenuItem(
+                    text = { Text("16 시간 후 알림") },
+                    onClick = { label = "16 시간 후 알림" },
+                )
+                Divider()
+                DropdownMenuItem(
+                    text = { Text("17 시간 후 알림") },
+                    onClick = { label = "17 시간 후 알림" },
+                )
+            }
         }
     }
 }
