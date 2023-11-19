e3376e6bc71e9ddbc615661c2564aed8ec6caf83
jaeho.5 <ovsoce@gmail.com>
2023-11-19 11:42:38 +0900

    feat: BottomBar UI 수정

--- app/src/main/java/io/github/ovso/fastingtime/MainScreen.kt
+++ app/src/main/java/io/github/ovso/fastingtime/MainScreen.kt
@@ -1,7 +1,7 @@
 package io.github.ovso.fastingtime
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +10,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -27,14 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @ExperimentalMaterial3Api
 @Composable
 fun MainScreen() {
-
+    val context = LocalContext.current
     var ctaBtnText by remember { mutableStateOf("시작") }
     var status by remember { mutableStateOf(false) }
     Scaffold(
@@ -42,24 +41,18 @@ fun MainScreen() {
             Spacer(modifier = Modifier.height(50.dp))
         },
         bottomBar = {
-            Box(
-                modifier = Modifier.fillMaxWidth()                        .imePadding()
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .imePadding()
                     .navigationBarsPadding(),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(
-                    modifier = Modifier
-                        .width(200.dp),
-                    onClick = {
-                        status = !status
-                        ctaBtnText = if (status) "멈춤" else "시작"
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Black
-                    )
-                ) {
-                    Text(text = ctaBtnText, style = MaterialTheme.typography.headlineLarge)
+                shape = RectangleShape,
+                onClick = {
+                    status = !status
+                    ctaBtnText = if (status) "멈춤" else "시작"
                 }
+            ) {
+                Text(text = ctaBtnText, style = MaterialTheme.typography.headlineLarge)
             }
         }
     ) {
@@ -74,12 +67,16 @@ fun MainScreen() {
             Spacer(modifier = Modifier.height(20.dp))
             TimeInput(
                 modifier = Modifier,
-                state = TimePickerState(0, 0, is24Hour = true)
+                state = TimePickerState(19, 0, is24Hour = true)
             )
             Divider(modifier = Modifier.padding(vertical = 40.dp))
-            TimeInput(state = TimePickerState(0, 0, is24Hour = true))
+            TimeInput(state = TimePickerState(15, 0, is24Hour = true))
             Text(text = "이후 공복 멈춤", style = MaterialTheme.typography.headlineLarge)
         }
+    }
+
+    BackHandler {
+        (context as? MainActivity)?.finish()
     }
 }
 
