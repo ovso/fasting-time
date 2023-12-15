22f896cf89eb6405ace490717bc83d7599c70423
jaeho.5 <ovsoce@gmail.com>
2023-12-15 23:31:31 +0900

    feat: alarmScheduler 에 사용할 alarmItem 설정

--- app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
+++ app/src/main/java/io/github/ovso/fastingtime/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -15,6 +14,8 @@ import io.github.ovso.fastingtime.alarm.AlarmItem
 import io.github.ovso.fastingtime.alarm.AlarmScheduler
 import io.github.ovso.fastingtime.alarm.AlarmSchedulerImpl
 import io.github.ovso.fastingtime.ui.theme.FastingtimeTheme
+import java.time.LocalDateTime
+import java.time.Month
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +24,11 @@ class MainActivity : ComponentActivity() {
 
         // https://medium.com/@nipunvirat0/how-to-schedule-alarm-in-android-using-alarm-manager-7a1c3b23f1bb
         val alarmScheduler: AlarmScheduler = AlarmSchedulerImpl(this)
-        var alarmItem: AlarmItem? = null
-
+        val alarmItem = AlarmItem(
+            alarmTime = LocalDateTime.of(2023, Month.DECEMBER, 15, 21, 35),
+            message = "message"
+        )
+        alarmScheduler.schedule(alarmItem)
         setContent {
             FastingtimeTheme {
                 // A surface container using the 'background' color from the theme
@@ -37,7 +41,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun PreviewMainActivity() {
